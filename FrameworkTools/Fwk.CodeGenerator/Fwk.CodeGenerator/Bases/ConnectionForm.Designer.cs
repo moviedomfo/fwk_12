418d34b730b178e5d5b1eb7df4e20cdff050e094
Marcelo Fabián <marcelo.oviedo@gmail.com>
2015-12-12 11:36:51 -0300

    Fwk.Database Resize de ConnectionForm

--- FrameworkTools/Fwk.CodeGenerator/Fwk.CodeGenerator/Bases/ConnectionForm.Designer.cs
+++ FrameworkTools/Fwk.CodeGenerator/Fwk.CodeGenerator/Bases/ConnectionForm.Designer.cs
@@ -34,31 +34,33 @@ namespace Fwk.CodeGenerator
             // 
             // cnnDataBaseForm1
             // 
-            this.cnnDataBaseForm1.ButtonsBackColor = System.Drawing.Color.SteelBlue;
-            this.cnnDataBaseForm1.ButtonsFlatStyle = System.Windows.Forms.FlatStyle.Standard;
+            this.cnnDataBaseForm1.ButtonsBackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cnnDataBaseForm1.ButtonsFlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cnnDataBaseForm1.ButtonsFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-       
+            this.cnnDataBaseForm1.ButtonsForeColor = System.Drawing.SystemColors.InfoText;
             this.cnnDataBaseForm1.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cnnDataBaseForm1.LabelsForeColor = System.Drawing.Color.MintCream;
+            this.cnnDataBaseForm1.LabelsForeColor = System.Drawing.SystemColors.InfoText;
             this.cnnDataBaseForm1.Location = new System.Drawing.Point(-1, -1);
+            this.cnnDataBaseForm1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cnnDataBaseForm1.Name = "cnnDataBaseForm1";
-            this.cnnDataBaseForm1.Size = new System.Drawing.Size(375, 195);
+            this.cnnDataBaseForm1.Size = new System.Drawing.Size(500, 240);
             this.cnnDataBaseForm1.TabIndex = 0;
             this.cnnDataBaseForm1.TestBottonVisible = true;
-            this.cnnDataBaseForm1.Load += new System.EventHandler(this.cnnDataBaseForm1_Load);
-            this.cnnDataBaseForm1.AceptEvent += new Fwk.DataBase.CustomControls.AceptHandler(this.cnnDataBaseForm1_AceptEvent);
             this.cnnDataBaseForm1.CancelEvent += new Fwk.DataBase.CustomControls.CancelHandler(this.cnnDataBaseForm1_CancelEvent);
+            this.cnnDataBaseForm1.AceptEvent += new Fwk.DataBase.CustomControls.AceptHandler(this.cnnDataBaseForm1_AceptEvent);
+            this.cnnDataBaseForm1.Load += new System.EventHandler(this.cnnDataBaseForm1_Load);
             // 
             // ConnectionForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.SteelBlue;
-            this.ClientSize = new System.Drawing.Size(380, 186);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(493, 236);
             this.Controls.Add(this.cnnDataBaseForm1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(388, 220);
-            this.MinimumSize = new System.Drawing.Size(388, 220);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(511, 400);
+            this.MinimumSize = new System.Drawing.Size(511, 200);
             this.Name = "ConnectionForm";
             this.Text = "Connection";
             this.ResumeLayout(false);
