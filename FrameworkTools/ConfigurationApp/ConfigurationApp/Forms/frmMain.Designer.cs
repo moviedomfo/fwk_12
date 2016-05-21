5edfa37d18d2e7b49261dec098d07e7187181b5a
Marcelo Fabián <marcelo.oviedo@gmail.com>
2016-05-21 10:58:05 -0300

    mejoras varias.. Bussines facade : mejoras en audit, Context information se le agrega cultura Helper Function mejoras en serializacion
    Se arregla Autorization rule . Se agrega Name
    Impersonate
    
    organizacion de proyectos..
     Post Builds en 7 proyectos
    Scriupt .bat para releases

--- FrameworkTools/ConfigurationApp/ConfigurationApp/Forms/frmMain.Designer.cs
+++ FrameworkTools/ConfigurationApp/ConfigurationApp/Forms/frmMain.Designer.cs
@@ -243,7 +243,7 @@ namespace ConfigurationApp
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(157, 11);
+            this.label1.Location = new System.Drawing.Point(200, 30);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(904, 20);
