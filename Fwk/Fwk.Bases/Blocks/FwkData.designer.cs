4d8f8d11a8aa105339aab401a4ab13cf02eac20f
Marcelo Fabián <marcelo.oviedo@gmail.com>
2016-11-17 09:02:02 -0300

    Improvements in dbo.fwk_ServiceAudit , simplefacade : Service audit improvements

--- Fwk/Fwk.Bases/Blocks/FwkData.designer.cs
+++ Fwk/Fwk.Bases/Blocks/FwkData.designer.cs
@@ -48,7 +48,7 @@ namespace Fwk.ConfigData
     #endregion
 		
 		public FwkDatacontext() : 
-				base(global::Fwk.Bases.Properties.Settings.Default.fwktest1ConnectionString, mappingSource)
+				base(global::Fwk.Bases.Properties.Settings.Default.fwktestConnectionString1, mappingSource)
 		{
 			OnCreated();
 		}
