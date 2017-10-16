8c56286f508ce552f29b5039c542a4260985a8cc
Marcelo F Oviedo <marcelo.oviedo@gmail.com>
2017-10-16 11:23:48 -0300

    merge con las libs de empresa

---
+++ Dispatchers/WebAPIDispatcherClienteWeb/Scripts/Index.js
@@ -0,0 +1,167 @@
+﻿$(function () {
+    var hostedRootPath = 'http://localhost:17854';
+    var hostedRootPath_webapi = 'http://localhost:47647';
+    var url = 'http://localhost:38091/SingleService.asmx' ;
+    var currentDate = new Date();
+
+    var contextInformation = new ContextInformation();
+    contextInformation.Culture = "ES-AR";
+    contextInformation.HostName = 'localhost';
+    contextInformation.HostIp = '10.10.200.168';
+    contextInformation.HostTime = currentDate;
+    contextInformation.ServerName = 'WebAPIDispatcherClienteWeb',
+    contextInformation.ServerTime = currentDate;
+    contextInformation.UserName = 'moviedo';
+    contextInformation.UserId = '';
+    contextInformation.AppId = 'WebAPIDispatcherClienteWeb';
+    contextInformation.ProviderName = 'health';
+
+    var requetObj = {
+        SecurityProviderName: null,
+        ServiceName: 'RetrivePatientsService',
+        BusinessData: {
+            Nombre: "Jimmy  L. ",
+            Apellido: "Hendryx",
+            NroDocumento: 25365441,
+            Id: 33
+            
+        },
+        ContextInformation: contextInformation
+    };
+
+    var data = {
+        providerName: requetObj.ContextInformation.ProviderName,
+        serviceName: requetObj.ServiceName,
+        jsonRequets: JSON.stringify(requetObj)
+    };
+    
+    $('#btnCallService_thisSite_POST_WebAPI').click(function () {
+        CallService_thisSite_POST_WebAPI();
+    });
+    $('#btnAjaxCall_POST_jsonp_WebAPI').click(function () {
+        Call_POST_jsonp_WebAPI();
+    });
+    $('#btnCallService_WS_Cruzado').click(function () {
+        CallService_WS_Cruzado();
+    });
+    $('#btnCallService_WS_Cruzado2').click(function () {
+        CallService_WS_Cruzado_FwkWebCLientWrapper();
+    });
+    function logResults(json) {
+        alert(json);
+    }
+
+    function Call_POST_jsonp_WebAPI() {
+        //$.ajax({
+        //    type: 'GET',
+      
+        //    url: 'http://localhost:47647' + '/api/SingleServiceApi/Ping/test123")',
+        //    //data: JSON.stringify(data),
+        //    contentType: 'application/json; charset=utf-8',
+        //    jsonp: "logResults",
+        //    dataType: "jsonp",
+        //    //crossDomain: true,
+        //    success: function (result) {
+        //        var resultParced = JSON.parse(result);
+        //        alert('llamada al servicio OK ' + result);
+        //    },
+        //    error: ServiceFailed
+        //});
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:47647' + '/api/SingleServiceApi/Execute/',
+            data: "pepe",//JSON.stringify(data),
+            contentType: 'application/json',
+            //jsonp: "logResults",
+            dataType: "jsonp",
+           // headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'POST' },
+            //processData: false,
+            //crossDomain: true,
+            success: function (result) {
+                var resultParced = JSON.parse(result);
+                alert('llamada al servicio OK ' + result);
+            },
+            error: ServiceFailed
+        });
+
+       
+    }
+
+    function CallService_thisSite_POST_WebAPI() {
+      
+        
+     
+        //Realiza un POST al propio server 
+        $.ajax({
+            type: 'POST',
+            url: hostedRootPath + '/api/HomeAPI/Execute/',
+            data:  JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (result) {
+                var resultParced = JSON.parse(result);
+                alert('llamada al servicio OK ' + result);
+            },
+            error: ServiceFailed
+        });
+
+
+    }
+
+    function CallService_WS_Cruzado() {
+     
+   
+        $.ajax({
+            
+            type: 'POST',
+            url: url + '/EjecutarTest',
+            dataType: 'jsonp',
+            
+            //contentType: "text/javascript; charset=utf-8",
+            data: data,
+            crossDomain: true,
+            jsonpCallback: 'CallbackResponse',
+            success: CallbackResponse,
+            error: function (pJqXHR, pTextStatus, pErrorThrown) {
+                alert(pJqXHR.responseText);
+                return null;
+            }
+        });
+        
+    }
+    function CallbackResponse(response) {
+        
+        var businessData = response.BusinessData;
+        alert('llamada al servicio OK ' + businessData[0].PatientId);
+    }
+  
+    function CallService_WS_Cruzado_FwkWebCLientWrapper() {
+
+        var wrapper = new FwkWebCLientWrapper();
+        wrapper.ExecuteService(url, requetObj, wrapper_Callback);
+
+      
+
+    }
+
+    function wrapper_Callback(response) {
+       
+        
+        if (response.error)
+        {
+            alert(response.error.MessageText);
+            return;
+        }
+        var businessData = response.BusinessData;
+        alert('llamada al servicio OK ');
+        alert(JSON.stringify( businessData));
+    }
+
+
+
+    function ServiceFailed(xhr, status, p3, p4) {
+        var errMsg = status + " " + p3;
+        errMsg = errMsg + "\n" + xhr.responseText;
+        alert(errMsg);
+    }
+});
--- FrameworkTools/Fwk.Logging.Viewer/ConnectionForm.Designer.cs
+++ FrameworkTools/Fwk.Logging.Viewer/ConnectionForm.Designer.cs
@@ -42,24 +42,26 @@ namespace Fwk.Logging.Viewer
             this.cnnDataBaseForm1.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cnnDataBaseForm1.LabelsForeColor = System.Drawing.Color.Black;
             this.cnnDataBaseForm1.Location = new System.Drawing.Point(-1, -1);
+            this.cnnDataBaseForm1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.cnnDataBaseForm1.Name = "cnnDataBaseForm1";
-            this.cnnDataBaseForm1.Size = new System.Drawing.Size(375, 195);
+            this.cnnDataBaseForm1.Size = new System.Drawing.Size(496, 228);
             this.cnnDataBaseForm1.TabIndex = 0;
             this.cnnDataBaseForm1.TestBottonVisible = true;
-            this.cnnDataBaseForm1.Load += new System.EventHandler(this.cnnDataBaseForm1_Load);
-            this.cnnDataBaseForm1.AceptEvent += new Fwk.DataBase.CustomControls.AceptHandler(this.cnnDataBaseForm1_AceptEvent);
             this.cnnDataBaseForm1.CancelEvent += new Fwk.DataBase.CustomControls.CancelHandler(this.cnnDataBaseForm1_CancelEvent);
+            this.cnnDataBaseForm1.AceptEvent += new Fwk.DataBase.CustomControls.AceptHandler(this.cnnDataBaseForm1_AceptEvent);
+            this.cnnDataBaseForm1.Load += new System.EventHandler(this.cnnDataBaseForm1_Load);
             // 
             // ConnectionForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(380, 186);
+            this.ClientSize = new System.Drawing.Size(493, 239);
             this.Controls.Add(this.cnnDataBaseForm1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(388, 220);
-            this.MinimumSize = new System.Drawing.Size(388, 220);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(611, 360);
+            this.MinimumSize = new System.Drawing.Size(511, 260);
             this.Name = "ConnectionForm";
             this.Text = "Connection";
             this.ResumeLayout(false);
